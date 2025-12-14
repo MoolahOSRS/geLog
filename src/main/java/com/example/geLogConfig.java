0318008f4f63260bb969520a2834d551c99ac2af
Moolah <67553387+MoolahOSRS@users.noreply.github.com>
2025-12-14 09:01:52 -0700

    Update geLogConfig.java

--- src/main/java/com/example/geLogConfig.java
+++ src/main/java/com/example/geLogConfig.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.geLogPlugin;
+package MoolahOSRS.geLog;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
@@ -27,4 +27,5 @@ public interface geLogConfig extends Config {
     {
         return true;
     }
+
 }
